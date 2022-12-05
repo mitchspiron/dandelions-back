a49b00bc99895de5af86ec274d2cea74b70ab6d7
mitchspiron <mitchspiron280@gmail.com>
2022-12-06 02:07:20 +0300

    fix: front url

--- dist/main.js
+++ dist/main.js
@@ -31,7 +31,7 @@ async function bootstrap() {
     }));
     app.use((0, express_flash_message_1.flash)({ sessionKeyName: 'flashMessage' }));
     app.use((req, res, next) => {
-        res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+        res.header('Access-Control-Allow-Origin', process.env.URL_FRONT);
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
         res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization');
         res.header('Access-Control-Allow-Credentials', 'true');
@@ -39,7 +39,7 @@ async function bootstrap() {
     });
     app.enableCors({
         allowedHeaders: 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization',
-        origin: 'http://localhost:8080',
+        origin: process.env.URL_FRONT,
     });
     await app.listen(process.env.PORT);
     console.log('🚀 Server started at http://127.0.0.1:' + process.env.PORT);
